6da106053e9ed4cc385c58b00888a14eaf965267
Kishor <kishor1124@gmail.com>
2020-12-14 13:50:55 +0530

    angular databinding

--- Angular Projects/my-second-app/src/app/app.module.ts
+++ Angular Projects/my-second-app/src/app/app.module.ts
@@ -7,16 +7,22 @@ import { PipeDemoComponent } from './pipe-demo/pipe-demo.component';
 import { LengthFinder, PersonTitle } from './pipe-demo/length-finder';
 import { XyzPipe } from './xyz.pipe';
 import { UserItemsComponent } from './user-items/user-items.component';
+import { EventBindComponent } from './event-bind/event-bind.component';
+import { PropertyBindComponent } from './property-bind/property-bind.component';
+import { TwoWayDataBindComponent } from './two-way-data-bind/two-way-data-bind.component';
+import { FormsModule } from '@angular/forms';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserItemComponent } from './user-item/user-item.component';
+import { ChildCounterComponent } from './child-counter/child-counter.component';
+import { ParentCounterComponent } from './parent-counter/parent-counter.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HelloComponent,
-    PipeDemoComponent,
-    LengthFinder, PersonTitle, XyzPipe, UserItemsComponent
+    AppComponent,    HelloComponent, PipeDemoComponent,
+    LengthFinder, PersonTitle, XyzPipe, UserItemsComponent, EventBindComponent, PropertyBindComponent, TwoWayDataBindComponent, UserListComponent, UserItemComponent, ChildCounterComponent, ParentCounterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
